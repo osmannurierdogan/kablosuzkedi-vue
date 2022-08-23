791f9bd5099fc50f1831d623ba3992ed39d00ac4
Osman Nuri Erdoğan <osmerd04@gmail.com>
2022-08-24 02:42:03 +0300

    Slot in Components files uploaded.

---
+++ 12-component-slots/src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+div.container
+  div.row.m-5
+    div.col-sm-12.card.d-flex.align-items-center.justify-content-between.p-3
+      ModalComponent
+        template(#title)
+          h3 Slot title
+        template(#content)
+          p Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, deserunt, magni dolores consectetur reiciendis temporibus doloremque.
+        template(#footer)
+          button.btn.btn-success.me-2 Save
+          button.btn.btn-danger Close
+        template(#default)
+          p This is default content.
+</template>
+
+<script>
+import ModalComponent from "@/components/ModalComponent";
+export default {
+  name: "App",
+  components: {
+    ModalComponent,
+  },
+};
+</script>
---
+++ 12-component-slots/src/components/ModalComponent.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div.header
+  slot(name="title")
+slot
+div.content.my-2.text-danger 
+  slot(name="content")
+div.footer
+  slot(name="footer")
+</template>
+<script>
+export default {
+  name: "ModalComponent",
+};
+</script>
